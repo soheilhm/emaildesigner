8f60c689e1e684db934d299a3f64a7aa329e9a00
soheil <soheil.h@bananatag.com>
2018-11-21 15:49:34 -0800

    basic block functionality without drag and drop

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -1,37 +1,108 @@
 import React, {Component} from 'react';
-import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
-
+import generateID from '../../common/uuid';
 
 class Canvas extends Component {
     state = {
+        currBlocks: []
+    }
 
-    };
-
-    onSortEnd = ({oldIndex, newIndex}) => {
-        this.setState({
-            items: arrayMove(this.state.items, oldIndex, newIndex),
+    componentDidMount() {
+        this.setState((prevState, props) => {
+            return {
+                currBlocks: this.props.currBlocks
+            }
         });
-    };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const { newBlock } = nextProps;
+        if (nextProps !== undefined) {
+            this.setState((prevState, props) => {
+                return {
+                    currBlocks: [...prevState.currBlocks, newBlock],
+                }
+            });
+        }
+    }
+
+    _duplicateBlock(block) {
+        this.setState((prevState, props) => {
+            const idx = prevState.currBlocks.indexOf(block);
+            return {
+                currBlocks: [
+                    ...prevState.currBlocks.slice(0, idx),
+                    {...block, index: generateID() },
+                    ...prevState.currBlocks.slice(idx)
+                ]
+            }
+        });
+    }
+
+    _moveUpBlock(block) {
+        this.setState((prevState, props) => {
+            const idx = prevState.currBlocks.indexOf(block);
+            if (idx === 0) {
+                return { currBlocks: [...prevState.currBlocks] };
+            }
+
+            const prevBlock = prevState.currBlocks[idx - 1];
+            return {
+                currBlocks: [
+                    ...prevState.currBlocks.slice(0, idx - 1),
+                    {...block, index: generateID() },
+                    {...prevBlock, index: generateID() },
+                    ...prevState.currBlocks.slice(idx + 1)
+                ]
+            }
+        });
+    }
+
+    _moveDownBlock(block) {
+        this.setState((prevState, props) => {
+            const idx = prevState.currBlocks.indexOf(block);
+            if (idx === prevState.currBlocks.length - 1) {
+                return { currBlocks: [...prevState.currBlocks] };
+            }
+
+            const nextBlock = prevState.currBlocks[idx + 1];
+            return {
+                currBlocks: [
+                    ...prevState.currBlocks.slice(0, idx),
+                    {...nextBlock, index: generateID() },
+                    {...block, index: generateID() },
+                    ...prevState.currBlocks.slice(idx + 2)
+                ]
+            }
+        });
+    }
+
+
+    _removeBlock(block) {
+        this.setState((prevState, props) => {
+            return {
+                currBlocks: prevState.currBlocks.filter((elm) => elm !== block)
+            }
+        });
+    }
+
     render() {
-        const blocks = this.props.blocks || [];
-        const SortableItem = SortableElement(({value}) =>
-            <li>{value}</li>
+        return (
+            <div className="canvas" style={{ width: '65%', backgroundColor: 'lightgray', float: 'right'}} >
+                <h3 style={{padding: '10px'}}>Canvas</h3>
+                <ul>
+                    {this.state.currBlocks.map((block, index) => (
+                        <div key={block.index} style={{padding: '10px',margin: '10px 0', border: '2px dashed #51d1fb', backgroundColor: block.color}}>
+                            <li style={{display: 'inline-block', width: '55%'}}>{block.content}</li>
+                            <button onClick={this._duplicateBlock.bind(this, block)} style={{display: 'inline-block', padding: '5px', margin: '0 5px'}}>Duplicate</button>
+                            <button onClick={this._moveUpBlock.bind(this, block)} style={{display: 'inline-block', padding: '5px', margin: '0 5px'}}>Move Up</button>
+                            <button onClick={this._moveDownBlock.bind(this, block)} style={{display: 'inline-block', padding: '5px', margin: '0 5px'}}>Move Down</button>
+                            <button onClick={this._removeBlock.bind(this, block)} style={{display: 'inline-block', padding: '5px', margin: '0 5px'}}>Delete</button>
+                        </div>
+
+                    ))}
+                </ul>
+            </div>
         );
-
-        const SortableList = SortableContainer(({items}) => {
-            return (
-                <div className="canvas" style={{ width: '65%', backgroundColor: 'lightgray', float: 'right'}} >
-                    <h3>Canvas</h3>
-                    <ul>
-                        {items.map((value, index) => (
-                            <SortableItem key={`item-${index}`} index={index} value={value} />
-                        ))}
-                    </ul>
-                </div>
-            );
-        });
-
-        return <SortableList items={blocks} onSortEnd={this.onSortEnd} />;
     }
 }
 export default Canvas;
--- src/components/Toolbox/Toolbox.js
+++ src/components/Toolbox/Toolbox.js
@@ -1,17 +1,63 @@
 import React, { Component } from "react";
+import generateID from '../../common/uuid';
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 class Toolbox extends Component {
     state = {
-        blocks: ['Block1', 'Block2', 'Block3', 'Block4']
+        blocks: [
+            { 
+                index: '311139e8-45a1-4815-8fc0-195ebe395ab6', 
+                type: 'Block1', 
+                content: 'Block1',
+                color: getRandomColor()
+            },
+            { 
+                index: 'ad56fb77-5617-401c-8fad-67b4cb481efb', 
+                type: 'Block2', 
+                content: 'Block2',
+                color: getRandomColor()
+            },
+            { 
+                index: '02f2a335-a295-4d8d-a3ed-6316dbc1604e', 
+                type: 'Block3', 
+                content: 'Block3',
+                color: getRandomColor()
+            },
+            { 
+                index: '9d339d39-3e75-4a17-b0ab-65af6d679a43', 
+                type: 'Block4', 
+                content: 'Block4',
+                color: getRandomColor()
+            }
+        ]
     };
-    render() {
 
+    _addBlock(block) {
+        this.props.onBlockAdd({
+            ...block,
+            index: generateID(),
+            color: getRandomColor()
+        });
+    }
+
+    render() {
         return (
-            <div className="toolbox" style={{ width: '30%', backgroundColor: 'lightgreen', float: 'left'}}>
-                <h3>Toolbox</h3>
+            <div className="toolbox" style={{ width: '30%', backgroundColor: 'lightblue', float: 'left'}}>
+                <h3 style={{padding: '10px'}}>Toolbox</h3>
                 <ul>
-                    {this.state.blocks.map((value, index) => (
-                        <li key={index}>{value} - Add</li>
+                    {this.state.blocks.map((block, index) => (
+                        <div key={block.index} style={{padding: '5px',margin: '5px 0', border: '1px solid gray'}}>
+                            <li style={{display: 'inline-block', width: '50%', padding: '5px', margin: '0 5px'}}>{block.type}</li>
+                            <button onClick={this._addBlock.bind(this, block)} style={{display: 'inline-block', padding: '10px', margin: '0 5px'}}>Add</button>
+                        </div>
                     ))}
                 </ul>
             </div>
